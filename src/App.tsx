9b0630fd40888e3857197efe1798ebb311a9670d
joeng03 <yinjoeng2003@gmail.com>
2023-02-01 19:54:11 +0800

    Configured basic structure, setup TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+
+const App = () => {
+  return <div>testing </div>;
+};
+
+export default App;
